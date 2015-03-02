aa3b057c894365d1fbaa30dabad24097ada9f526
aaschaefer <aaschaef@uncg.edu>
2015-03-02 07:50:14 -0500

    Initial Project

---
+++ app/src/main/java/csc495/androidbeamdemo/MainActivity.java
@@ -0,0 +1,104 @@
+package csc495.androidbeamdemo;
+
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.NfcAdapter.CreateNdefMessageCallback;
+import android.nfc.NfcEvent;
+import android.os.Build;
+import android.os.Parcelable;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity implements CreateNdefMessageCallback {
+
+    NfcAdapter nfcAdapter;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.textView);
+
+        //Check for available NFC Adapter
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        if(nfcAdapter == null) {
+            Toast.makeText(this, "NFC is not available", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+
+        if(!nfcAdapter.isEnabled()) {
+            Toast.makeText(this, "NFC is not enabled", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+
+        if(!nfcAdapter.isNdefPushEnabled()) {
+
+            Toast.makeText(this, "NFC is not push enabled", Toast.LENGTH_SHORT).show();
+
+        } else {
+            //Register callback
+            nfcAdapter.setNdefPushMessageCallback(this, this);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onResume() {
+        super.onResume();
+        if(NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
+            Toast.makeText(this, "Received beam", Toast.LENGTH_SHORT).show();
+            processIntent(getIntent());
+        }
+    }
+
+    public void onNewIntent(Intent intent) {
+        setIntent(intent);
+    }
+
+    @Override
+    public NdefMessage createNdefMessage(NfcEvent event) {
+        String text = "Hello World!";
+        NdefMessage msg = new NdefMessage(new NdefRecord[] {
+                NdefRecord.createMime("application/vnd.csc495.androidbeamdemo",text.getBytes())
+        });
+        return msg;
+    }
+
+    private void processIntent(Intent intent) {
+        Parcelable[] rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+        NdefMessage msg = (NdefMessage) rawMsgs[0];
+        textView = (TextView) findViewById(R.id.textView);
+        textView.setText(new String(msg.getRecords()[0].getPayload()));
+    }
+}
